73cc0f7926fbd17033a8308064171a0ad3da69c2
Eric Cageman <cagedev@gmail.com>
2018-06-21 17:00:38 +0200

    Placeholders voor maakSet() en isSet() gemaakt; extra uitleg in comments

--- src/game.py
+++ src/game.py
@@ -47,23 +47,31 @@ class Game():
     def isSet(self, kaart1, kaart2, kaart3):
         """
         Returnt de waarde True als kaart1, kaart2 en kaart3 een set vormen en anders returnt het de waarde False.
+        Om mee te beginnen zal het elke 3 kaarten als niet een set aanduiden (met return False).
         """
-#        if self.deelSet( kaart1['hoeveelheid'], kaart2['hoeveelheid'], kaart3['hoeveelheid'] ):
-        if self.deelSet( kaart1.hoeveelheid, kaart2.hoeveelheid, kaart3.hoeveelheid ):
-            if self.deelSet( kaart1['kleur'], kaart2['kleur'], kaart3['kleur'] ):
-                if self.deelSet( kaart1['vorm'], kaart2['vorm'], kaart3['vorm'] ):
-                    if self.deelSet( kaart1['vulling'], kaart2['vulling'], kaart3['vulling'] ):
-                        return True
-        else:
-            return False
+        return False
 
 
     def maakSet(self, kaart1, kaart2):
         """
-        Returnt de vereiste kaart om een set te maken.
+        Returnt de vereiste kaart om een set te maken. De eigenschappen van de kaarten kun je opvragen met:
+            kaart1.hoeveelheid, kaart1.kleur, kaart1.vorm, kaart1.vulling en
+            kaart2.hoeveelheid, kaart2.kleur, kaart2.vorm, kaart2.vulling en
+        Als bijvoorbeeld
+            maakSet( SetKaart(1,1,1,1), SetKaart(2,2,2,2) ) aangeroepen wordt dan geldt:
+                kaart1.hoeveelheid = 1
+                kaart1.kleur = 1
+                kaart1.vorm = 1
+                kaart1.vulling = 1
+                en
+                kaart2.hoeveelheid = 2
+                kaart2.kleur = 2
+                kaart2.vorm = 2
+                kaart2.vulling = 2
         """
-        return SetKaart( kaart1['hoeveelheid'] if kaart1['hoeveelheid'] == kaart2['hoeveelheid']  else (6-kaart1['hoeveelheid'] )-kaart2['hoeveelheid'],
-                         kaart1['kleur']       if kaart1['kleur']       == kaart2['kleur']        else (6-kaart1['kleur']       )-kaart2['kleur'],
-                         kaart1['vorm']        if kaart1['vorm']        == kaart2['vorm']         else (6-kaart1['vorm']        )-kaart2['vorm'],
-                         kaart1['vulling']     if kaart1['vulling']     == kaart2['vulling']      else (6-kaart1['vulling']     )-kaart2['vulling'] )
-###        return SetKaart(1, 1, 1, 1)
+        hv = 0
+        kl = 0
+        vo = 0
+        vu = 0
+
+        return SetKaart(hv, kl, vo, vu)
